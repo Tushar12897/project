fcd826c7f7c34b419fffb9299b778333732d8eef
Tushar12897 <86527727+Tushar12897@users.noreply.github.com>
2023-10-22 18:58:35 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,7 +9,6 @@ class Person(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
     gender = models.CharField(max_length=40,null=False)
-    subject = models.CharField(max_length=40,null=True,blank=True)
 
 
     
