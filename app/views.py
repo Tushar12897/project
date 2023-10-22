604fd871d0f085c1881729fbb6b80ceb1907a907
Tushar <tushar12897@gmail.com>
2023-10-22 22:07:36 +0530

    dfsf

--- app/views.py
+++ app/views.py
@@ -1,32 +1,32 @@
-    from urllib.request import Request
-    from django.shortcuts import render
+from urllib.request import Request
+from django.shortcuts import render
 
-    # Create your views here.
-    from django.shortcuts import render
-    from .models import  Person
+# Create your views here.
+from django.shortcuts import render
+from .models import  Person
 
-    #def home(request):
-    
-    #   return render(request,'index.html')
+#def home(request):
+   
+ #   return render(request,'index.html')
 
-    def save(request):   
-        if request.method == "POST":
-            firstname =request.POST.get("firstname")
-            lastname =request.POST.get("lastname")
-            subject = request.POST.get('subject')
-            gender = request.POST.get('gender')
-            birthday = request.POST.get('birthday')
-            email = request.POST.get('email')
-            mob = request.POST.get('mob')
+def save(request):   
+    if request.method == "POST":
+        firstname =request.POST.get("firstname")
+        lastname =request.POST.get("lastname")
+        subject = request.POST.get('subject')
+        gender = request.POST.get('gender')
+        birthday = request.POST.get('birthday')
+        email = request.POST.get('email')
+        mob = request.POST.get('mob')
 
-            
-        
-            reg = Person(firstname=firstname, lastname=lastname,subject=subject,gender=gender,birthday=birthday,email=email,mob=mob)
-            reg.save()
-            print('Data Save Successfully')
-        return render(request,'index.html')
+          
+       
+        reg = Person(firstname=firstname, lastname=lastname,subject=subject,gender=gender,birthday=birthday,email=email,mob=mob)
+        reg.save()
+        print('Data Save Successfully')
+    return render(request,'index.html')
 
-    
+  
 
 
 
